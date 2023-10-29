59ed5db8e34302026ef2d1dbb5bc4c724a813a0b
James910066 <95539722+James910066@users.noreply.github.com>
2023-10-29 19:30:03 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,112 +1,137 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files; //Imports file. Uses built in methods for working the actual files and directories and creating input and output streams
-import java.nio.file.Path; //Imports the path. Needed for creation of hierarchy of file path. Provides info about file location, size, type and permissions etc.
-import static java.nio.file.StandardOpenOption.CREATE;
+import java.io.*;
+import java.nio.file.*;
 import java.util.*;
-import javax.swing.JFileChooser;
-public class Main
-{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
     public static void main(String[] args) {
 
         JFileChooser fileChooser = new JFileChooser();
-        File chosenFile;
+        File chosenFile = null;
         String readLine = "";
         ArrayList<String> stopWords = new ArrayList<>();
         Map<String, Integer> wordFrequency = new TreeMap<>();
-        int lineCounter = 0;
         Set<String> keyWords = new TreeSet<>();
-        boolean done = false;
-        boolean finalConfirm = false;
         Scanner in = new Scanner(System.in);
 
+        try {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            fileChooser.setCurrentDirectory(workingDirectory);
 
+            // Choose and read the file with stop words
+            if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                chosenFile = fileChooser.getSelectedFile();
+                Path file = chosenFile.toPath();
+                InputStream stream = new BufferedInputStream(Files.newInputStream(file));
+                BufferedReader fileReader = new BufferedReader(new InputStreamReader(stream));
 
-            try //Start of Try block with program in it
-            {
-                File workingDirectory = new File(System.getProperty("user.dir"));
-                fileChooser.setCurrentDirectory(workingDirectory);
-                if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-                {
-                    chosenFile = fileChooser.getSelectedFile();
-                    Path file = chosenFile.toPath();
-                    InputStream stream = new BufferedInputStream(Files.newInputStream(file, CREATE));
-                    BufferedReader fileReader = new BufferedReader(new InputStreamReader(stream));
-                    final String fileName = chosenFile.getName();
-
-                    //Read the Stop Words file into an array to check against
-                    while (fileReader.ready()) {
-                        readLine = fileReader.readLine();
-                        stopWords.add(readLine);
-                    }
-                    for (String w : stopWords) {
-                        System.out.println(w);
-                    }
-
-                    System.out.println("Extracted files from: " + fileName);
-                    fileReader.close();
-                    System.out.println("\n\nData file read!");
-                }
-                else
-                {
-                    System.out.println("Failed to choose a file to process");
-                    System.out.println("Run the program again!");
-                    System.exit(0);
+                while (fileReader.ready()) {
+                    readLine = fileReader.readLine();
+                    stopWords.add(readLine);
                 }
 
-                //Choose and read the article to check for keyWords into an array
-                new File(System.getProperty("user.dir"));
-                fileChooser.setCurrentDirectory(workingDirectory);
-                if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-                {
-                    chosenFile = fileChooser.getSelectedFile();
-                    Path file = chosenFile.toPath();
-                    InputStream stream = new BufferedInputStream(Files.newInputStream(file, CREATE));
-                    BufferedReader fileReader = new BufferedReader(new InputStreamReader(stream));
-                    final String fileName = chosenFile.getName();
-
-                    //Read the Stop Words file into an array to check against
-                    while (fileReader.ready()) {
-                        readLine = fileReader.readLine();
-                        keyWords.add(readLine);
-                    }
-                    //Remove stop words and add keywords into array
-                    for (String w : keyWords)
-                    {
-                        
-                        System.out.println(w);
-                    }
-
-                    System.out.println("Extracted files from: " + fileName);
-                    fileReader.close();
-                    System.out.println("\n\nData file read!");
-                }
-                else
-                {
-                    System.out.println("Failed to choose a file to process");
-                    System.out.println("Run the program again!");
-                    System.exit(0);
-                }
-            }
-
-            catch (FileNotFoundException e)
-            {
-                e.printStackTrace();
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            System.out.println("Do you want to quit?");
-            finalConfirm = SafeInput.getYNConfirm(in, "Are you sure?");
-            if (finalConfirm)
-            {
+                System.out.println("Extracted stop words from: " + chosenFile.getName());
+                fileReader.close();
+                System.out.println("\n\nData file read!");
+            } else {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
                 System.exit(0);
             }
 
+            // Choose and read the second file with keywords
+            fileChooser.setCurrentDirectory(workingDirectory);
+            if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                chosenFile = fileChooser.getSelectedFile();
+                Path file = chosenFile.toPath();
+                InputStream stream = new BufferedInputStream(Files.newInputStream(file));
+                BufferedReader fileReader = new BufferedReader(new InputStreamReader(stream));
+
+                while (fileReader.ready()) {
+                    readLine = fileReader.readLine();
+                    String[] words = readLine.split("\\s+");
+                    for (String word : words) {
+                        word = word.toLowerCase(); // Convert word to lowercase
+                        // Exclude words with digits or punctuation
+                        if (!stopWords.contains(word) && word.matches("\\p{Alpha}+")) {
+                            keyWords.add(word);
+                        }
+                    }
+                }
+
+                System.out.println("Extracted files from: " + chosenFile.getName());
+                fileReader.close();
+                System.out.println("\n\nData file read!");
+            } else {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+
+            // Count frequency of keywords in the second file
+            for (String keyword : keyWords) {
+                int frequency = 0;
+                for (String line : Files.readAllLines(chosenFile.toPath())) {
+                    frequency += Arrays.asList(line.split("\\s+")).stream()
+                            .filter(word -> word.toLowerCase().equals(keyword)).count();
+                }
+                wordFrequency.put(keyword, frequency);
+            }
+
+            // Display the tags and their frequency in a JTextArea
+            JTextArea textArea = new JTextArea();
+            textArea.setEditable(false);
+
+            for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+                textArea.append(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            JFrame frame = new JFrame("Tag Frequencies");
+            frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+            frame.setSize(400, 300);
+            frame.setVisible(true);
+
+            // Allow user to save the extracted tags and frequencies
+            JButton saveButton = new JButton("Save to File");
+            saveButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser saveFileChooser = new JFileChooser();
+                    if (saveFileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                        File saveFile = saveFileChooser.getSelectedFile();
+                        try (PrintWriter writer = new PrintWriter(saveFile)) {
+                            for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+                                writer.println(entry.getKey() + ": " + entry.getValue());
+                            }
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+            JButton quitButton = new JButton("Quit");
+            quitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+                }
+            });
+
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.add(saveButton);
+            buttonPanel.add(quitButton);
+
+            frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
